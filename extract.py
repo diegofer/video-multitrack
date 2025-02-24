2ea3eefc3306cd50ce8a3d834891e736bb1de38c
diegofer <diegofernando83@gmail.com>
2025-02-23 19:13:02 -0500

    Refactor and implement threading for video and extract processing

---
+++ extract.py
@@ -0,0 +1,90 @@
+from PySide6.QtCore import QThread, Signal
+import os
+import sys
+import torchaudio
+import torch
+import soundfile as sf
+from demucs.apply import apply_model
+from demucs.pretrained import get_model
+from pathlib import Path
+import subprocess
+import unicodedata
+
+
+class TracksExtractThread(QThread):
+    result = Signal(list)
+
+    def __init__(self, video_path):
+        super().__init__()
+        self.video_path = video_path
+
+    def run(self):
+        self.process_video(self.video_path)
+
+    def process_video(self, input_mp4):
+        output_base_folder = "library"
+        
+        def get_ffmpeg_path():
+            return "ffmpeg"  # Asume que ffmpeg está en el PATH del sistema
+        
+        def delete_wav_files(folder):
+            for file in os.listdir(folder):
+                if file.endswith(".wav"):
+                    os.remove(os.path.join(folder, file))
+                    print(f"Eliminado: {file}")
+        
+        def normalize_filename(filename):
+            nfkd_form = unicodedata.normalize('NFKD', filename)
+            return "".join([c for c in nfkd_form if not unicodedata.combining(c)]).upper()
+        
+        output_folder_name = normalize_filename(os.path.splitext(input_mp4)[0])
+        output_folder = os.path.join(output_base_folder, output_folder_name)
+        Path(output_folder).mkdir(parents=True, exist_ok=True)
+        
+        output_wav = os.path.join(output_folder, "extracted_audio.wav")
+        
+        print("Extrayendo audio del archivo MP4...")
+        ffmpeg_path = get_ffmpeg_path()
+        cmd = [ffmpeg_path, "-i", input_mp4, "-vn", "-acodec", "pcm_s16le", "-ar", "44100", output_wav]
+        subprocess.run(cmd, check=True)
+        
+        print("Cargando modelo Demucs...")
+        model = get_model("htdemucs_6s")
+        
+        waveform, sample_rate = torchaudio.load(output_wav)
+        
+        if waveform.shape[0] == 1:
+            waveform = waveform.repeat(2, 1)
+        
+        waveform = waveform.unsqueeze(0)
+        
+        print("Separando pistas...")
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        print(f"Usando dispositivo: {device}")
+        separated = apply_model(model, waveform, split=True, overlap=0.25, device=device, segment=7.8)
+        
+        stem_names = model.sources
+        separated = separated.squeeze(0)
+        stem_names = ["drums", "bass", "other", "vocals", "piano", "guitar"]
+        sample_rate = 44100
+        
+        for i, stem in enumerate(stem_names):
+            output_wav_path = os.path.join(output_folder, f"{stem}.wav")
+            output_ogg_path = os.path.join(output_folder, f"{stem}.ogg")
+            audio_data = separated[i].cpu().numpy().T
+            
+            sf.write(output_wav_path, audio_data, sample_rate)
+            print(f"Guardado: {output_wav_path}")
+            
+            cmd = [ffmpeg_path, "-i", output_wav_path, "-c:a", "libvorbis", output_ogg_path, "-y"]
+            subprocess.run(cmd, check=True)
+            print(f"Convertido a OGG: {output_ogg_path}")
+        
+        output_extracted_ogg = os.path.join(output_folder, "full.ogg")
+        cmd = [ffmpeg_path, "-i", output_wav, "-c:a", "libvorbis", "-q:a", "10", output_extracted_ogg, "-y"]
+        subprocess.run(cmd, check=True)
+        print(f"Convertido a OGG: {output_extracted_ogg}")
+        
+        delete_wav_files(output_folder)
+        
+        print("Proceso finalizado.")
--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-import os
 import sys
-import torchaudio
-import torch
-import soundfile as sf
-from demucs.apply import apply_model
-from demucs.pretrained import get_model
-from pathlib import Path
-import subprocess
-import unicodedata
 from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget
 from PySide6.QtCore import Qt
+from extract import TracksExtractThread
 
-class DragDropWindow(QMainWindow):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Arrastrar y Soltar Video")
@@ -39,78 +31,19 @@ class DragDropWindow(QMainWindow):
             for url in event.mimeData().urls():
                 file_path = url.toLocalFile()
                 if file_path.endswith(".mp4"):
-                    self.process_video(file_path)
+                    self.load_video(file_path)
     
-    def process_video(self, input_mp4):
-        output_base_folder = "library"
-        
-        def get_ffmpeg_path():
-            return "ffmpeg"  # Asume que ffmpeg está en el PATH del sistema
-        
-        def delete_wav_files(folder):
-            for file in os.listdir(folder):
-                if file.endswith(".wav"):
-                    os.remove(os.path.join(folder, file))
-                    print(f"Eliminado: {file}")
-        
-        def normalize_filename(filename):
-            nfkd_form = unicodedata.normalize('NFKD', filename)
-            return "".join([c for c in nfkd_form if not unicodedata.combining(c)]).upper()
-        
-        output_folder_name = normalize_filename(os.path.splitext(input_mp4)[0])
-        output_folder = os.path.join(output_base_folder, output_folder_name)
-        Path(output_folder).mkdir(parents=True, exist_ok=True)
-        
-        output_wav = os.path.join(output_folder, "extracted_audio.wav")
-        
-        print("Extrayendo audio del archivo MP4...")
-        ffmpeg_path = get_ffmpeg_path()
-        cmd = [ffmpeg_path, "-i", input_mp4, "-vn", "-acodec", "pcm_s16le", "-ar", "44100", output_wav]
-        subprocess.run(cmd, check=True)
-        
-        print("Cargando modelo Demucs...")
-        model = get_model("htdemucs_6s")
-        
-        waveform, sample_rate = torchaudio.load(output_wav)
-        
-        if waveform.shape[0] == 1:
-            waveform = waveform.repeat(2, 1)
-        
-        waveform = waveform.unsqueeze(0)
-        
-        print("Separando pistas...")
-        device = "cuda" if torch.cuda.is_available() else "cpu"
-        print(f"Usando dispositivo: {device}")
-        separated = apply_model(model, waveform, split=True, overlap=0.25, device=device, segment=7.8)
-        
-        stem_names = model.sources
-        separated = separated.squeeze(0)
-        stem_names = ["drums", "bass", "other", "vocals", "piano", "guitar"]
-        sample_rate = 44100
-        
-        for i, stem in enumerate(stem_names):
-            output_wav_path = os.path.join(output_folder, f"{stem}.wav")
-            output_ogg_path = os.path.join(output_folder, f"{stem}.ogg")
-            audio_data = separated[i].cpu().numpy().T
-            
-            sf.write(output_wav_path, audio_data, sample_rate)
-            print(f"Guardado: {output_wav_path}")
-            
-            cmd = [ffmpeg_path, "-i", output_wav_path, "-c:a", "libvorbis", output_ogg_path, "-y"]
-            subprocess.run(cmd, check=True)
-            print(f"Convertido a OGG: {output_ogg_path}")
-        
-        output_extracted_ogg = os.path.join(output_folder, "full.ogg")
-        cmd = [ffmpeg_path, "-i", output_wav, "-c:a", "libvorbis", "-q:a", "10", output_extracted_ogg, "-y"]
-        subprocess.run(cmd, check=True)
-        print(f"Convertido a OGG: {output_extracted_ogg}")
-        
-        delete_wav_files(output_folder)
-        
-        print("Proceso finalizado.")
+    def load_video(self, fileName=None):
+        self.extract_thread = TracksExtractThread(fileName)
+        self.extract_thread.result.connect(self.on_extracted)
+        self.extract_thread.start()
+
+    def on_extracted():
+        print("Terminado y podemos Seguir con metadatos")
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = DragDropWindow()
+    window = MainWindow()
     window.show()
     sys.exit(app.exec())
